e9e9b66d65839df0f307ae638711fc4a4ed2b267
ankush-phulia <ankush.phulia22@gmail.com>
2018-08-22 14:00:55 +0530

    Plot script now also gives log plot

--- Assignment 1 - FP Trees/scripts/run.sh
+++ Assignment 1 - FP Trees/scripts/run.sh
@@ -3,7 +3,7 @@ if [ -f fpm ]; then
     if [ "$#" == 4 ]; then
         ./fpm "$1" "$2" "$3" "$4"
     elif [ "$#" == 2 ] && [ "$2" == "-plot" ]; then
-        python plot.py "$1" --supports 5 10 25 50 90
+        python plot.py "$1" --supports 1 5 10 25 50 90
     else
         echo "Illegal number of parameters ($#), should be 4"
     fi
