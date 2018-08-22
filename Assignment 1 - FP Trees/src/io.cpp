dad8430b525574ef2e4464de0eccce05bc634497
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-08-22 12:55:28 +0530

    Modify parseLine functions to correct implementation issues in io.cpp
    This commit modifies the implementation of the parseLine functions to
    take care of spaces while reading the characters. This also modifies the
    lookup in unordered_map from find() to count().

--- Assignment 1 - FP Trees/src/io.cpp
+++ Assignment 1 - FP Trees/src/io.cpp
@@ -10,12 +10,21 @@ bool parseLineVec(FILE* inFile, vector<item> &transaction) {
     if (c == EOF) {
         return false;
     }
+
+    int n = 0;
+    bool done = false;
+
     while (c != '\n') {
-        int n = 0;
-        for (; (c > 47 && c < 58); c = fgetc_unlocked(inFile)) {
-            n = n * 10 + c - 48;
+        if (c > 47 && c < 58) {
+            done = false;
+            n = (n << 3) + (n << 1) + c - 48;
+        } 
+        else if (!done) {
+            transaction.emplace_back(n);
+            n = 0;
+            done = true;
         }
-        transaction.emplace_back(n);
+
         c = fgetc_unlocked(inFile);
     }
     return true;
@@ -26,14 +35,24 @@ bool parseLineVecInitial(FILE* inFile, unordered_map<item, int> &priorityMap) {
     if (c == EOF) {
         return false;
     }
+
+    int n = 0;
+    bool done = false;
+    
     while (c != '\n') {
-        int n = 0;
-        for (; (c > 47 && c < 58); c = fgetc_unlocked(inFile)) {
-            n = n * 10 + c - 48;
+        if (c > 47 && c < 58) {
+            done = false;
+            n = (n << 3) + (n << 1) + c - 48;
+        } 
+        else if (!done) {
+            priorityMap[n] += 1;
+            n = 0;
+            done = true;
         }
-        priorityMap[n] += 1;
+
         c = fgetc_unlocked(inFile);
     }
+
     return true;
 }
 
@@ -43,16 +62,25 @@ bool parseLineVecFiltered(FILE* inFile, vector<item> &transaction,
     if (c == EOF) {
         return false;
     }
+
+    int n = 0;
+    bool done = false;
+
     while (c != '\n') {
-        int n = 0;
-        for (; (c > 47 && c < 58); c = fgetc_unlocked(inFile)) {
-            n = n * 10 + c - 48;
+        if (c > 47 && c < 58) {
+            done = false;
+            n = (n << 3) + (n << 1) + c - 48;
+        } 
+        else if (!done) {
+            if (priorityMap.count(n))
+                transaction.emplace_back(n);
+            n = 0;
+            done = true;
         }
 
-        if (priorityMap.find(n) != priorityMap.end())
-            transaction.emplace_back(n);
         c = fgetc_unlocked(inFile);
     }
+
     return true;
 }
 
