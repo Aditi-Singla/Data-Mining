867b55ad8662c4c126e131ad212169fb57fd8f83
Aditi-Singla <aditisksingla@gmail.com>
2018-09-17 20:33:03 +0530

    Update kmeans for the new point

--- Assignment 2 - Clustering/include/point.h
+++ Assignment 2 - Clustering/include/point.h
@@ -14,4 +14,33 @@ typedef bg::model::point<double, MAX_DIM, bg::cs::cartesian> point;
 typedef bg::model::box<point> box;
 typedef std::pair<point, pId> value;
 
+inline void setPoint(point &pt, std::vector<double> &attributes) {
+
+	//TODO: Any elegant way to do this?
+	if (attributes.size() == 0){
+		attributes.push_back(pt.get<0>());
+		attributes.push_back(pt.get<1>());
+		attributes.push_back(pt.get<2>());
+		attributes.push_back(pt.get<3>());
+		attributes.push_back(pt.get<4>());
+	}
+	else{
+		attributes[0] = pt.get<0>();
+		attributes[1] = pt.get<1>();
+		attributes[2] = pt.get<2>();
+		attributes[3] = pt.get<3>();
+		attributes[4] = pt.get<4>();
+	}
+}
+
+inline point getPoint(std::vector<double> &attributes) {
+	point pt;
+	pt.set<0>(attributes[0]);
+	pt.set<1>(attributes[1]);
+	pt.set<2>(attributes[2]);
+	pt.set<3>(attributes[3]);
+	pt.set<4>(attributes[4]);
+	return pt;
+}
+
 #endif
--- Assignment 2 - Clustering/src/clustering.cpp
+++ Assignment 2 - Clustering/src/clustering.cpp
@@ -2,25 +2,12 @@
 
 using namespace std;
 
-inline point setPoint(vector<double> &attributes) {
-	point pt;
-	// for (int i = 0; i < MAX_DIM; ++i) {
-	// 	pt.set<i>(attributes[i]);
-	// }
-	pt.set<0>(attributes[0]);
-	pt.set<1>(attributes[1]);
-	pt.set<2>(attributes[2]);
-	pt.set<3>(attributes[3]);
-	pt.set<4>(attributes[4]);
-	return pt;
-}
-
 void clustering::readData(string &inFile) {
     FILE* inputStream = fopen(inFile.c_str(), "r");
     vector<double> attributes(MAX_DIM, 0);
     int i = 0;
     while (parseLine(inputStream, attributes, dim)) {
-        points.push_back(make_pair(setPoint(attributes), i++));
+        points.push_back(make_pair(getPoint(attributes), i++));
     }
     fclose(inputStream);
     
