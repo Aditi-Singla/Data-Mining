76956cf109a1f9e2b4a10d0f7cd75a93414b713e
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-09-19 12:16:08 +0530

    Add type for point object in OPTICS

--- Assignment 2 - Clustering/include/optics.h
+++ Assignment 2 - Clustering/include/optics.h
@@ -1,8 +1,16 @@
 #ifndef OPTICS_H
 #define OPTICS_H
 
+#include <limits>
+
 #include "io.h"
 #include "clustering.h"
+
+#define REACHABILITY_DISTANCE_UNDEFINED std::numeric_limits<double>::max()
+
+typedef std::pair<double, int> POINT_OBJECT;
+
+#define MAKE_POINT_OBJECT(dist, index) std::make_pair(dist, index)
 
 class optics : private clustering {
 		rtree rTree;
@@ -11,6 +19,7 @@ class optics : private clustering {
         optics(std::string &inFileName);
         void getReachability(int minPts, double maxEps);
         std::vector<cId> getClusters(int minPts, double maxEps);
+        bool expandCluster(int startPtIndex, cId clusterID, double eps, int minPts);
 
     private :
         std::vector<double> reachabilityDistances;
